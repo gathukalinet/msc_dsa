8083d63f1893149a69ebd2325472dec8395262c6
Linet Gathuka <lynettg05@gmail.com>
2025-06-17 12:47:25 +0300

    removed hidden duplicated columns

--- msc_dsa_data.py
+++ msc_dsa_data.py
@@ -65,29 +65,36 @@ intake_gender_2023 = intake_gender_2023.loc[:, ~intake_gender_2023.columns.dupli
 
 completion_rate_2022 = df3
 completion_rate_2022 = completion_rate_2022.iloc[:4,]
+completion_rate_2022 = completion_rate_2022.loc[:, ~completion_rate_2022.columns.duplicated()]
 
 completion_rate_2023 = df4
 completion_rate_2023 = completion_rate_2023.iloc[:4,]
+completion_rate_2023 = completion_rate_2023.loc[:, ~completion_rate_2023.columns.duplicated()]
 
 overall_graduands_2025 = df5
 overall_graduands_2025 = overall_graduands_2025.iloc[:4, :]
+overall_graduands_2025 = overall_graduands_2025.loc[:, ~overall_graduands_2025.columns.duplicated()]
 
 grads_2025_gender = df6
 grads_2025_gender = grads_2025_gender.iloc[:4, :3]
+grads_2025_gender = grads_2025_gender.loc[:, ~grads_2025_gender.columns.duplicated()]
 
 msc_dsa_grad_list = df7
 msc_dsa_grad_list = msc_dsa_grad_list.loc[:, ~msc_dsa_grad_list.columns.duplicated()]
-
+msc_dsa_grad_list = msc_dsa_grad_list.loc[:, ~msc_dsa_grad_list.columns.duplicated()]
 
 pending_students_2023 = df8
 pending_students_2023 = pending_students_2023.iloc[:4, :5]
+pending_students_2023 = pending_students_2023.loc[:, ~pending_students_2023.columns.duplicated()]
 
 df9.columns = df9.iloc[0, :].values
 df9 = df9.iloc[1:]
 msc_dsa_pending_current = df9
+msc_dsa_pending_current = msc_dsa_pending_current.loc[:, ~msc_dsa_pending_current.columns.duplicated()]
 
 
 backlog_2024 = df10
+backlog_2024 = backlog_2024.loc[:, ~backlog_2024.columns.duplicated()]
 
 
 # Correcting df11 headers
@@ -96,15 +103,16 @@ df11.loc[-1] = df11.columns           # Add current column names as a new row
 df11.index = df11.index + 1          # Shift all existing rows down
 df11 = df11.sort_index()
 backlog_2023 = df11
+backlog_2023 = backlog_2023.loc[:, ~backlog_2023.columns.duplicated()]
 
 
 # Visualization based on selected table
 if selected_table == "Table 1":
     st.subheader("Total Graduands")
     st.write(total_graduands)
-# elif selected_table == "Table 2":
-#     st.subheader("Intake by gender (2023)")
-#     st.write(intake_gender_2023)
+elif selected_table == "Table 2":
+    st.subheader("Intake by gender (2023)")
+    st.write(intake_gender_2023)
 # elif selected_table == "Table 3":
 #     st.subheader("Completion Rate (2022)")
 #     st.write(completion_rate_2022)
