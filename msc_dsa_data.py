f9c2f2200315c54c9209ea54f3ec0c86c419e57f
Linet Gathuka <lynettg05@gmail.com>
2025-06-17 12:25:50 +0300

    adding the tables contents

--- msc_dsa_data.py
+++ msc_dsa_data.py
@@ -46,8 +46,7 @@ for i in range(1, 12):
     globals()[f'df{i}'] = df       # Creating. individual dataframes df1, df2, ..., df15
 
 # Changing df9 to have correct headers
-df9.columns = df9.iloc[0, :].values
-df9 = df9.iloc[1:]
+
 
 # Correcting df11 headers
 df11 = df11.loc[:, ~df11.columns.duplicated()]
@@ -57,18 +56,80 @@ df11 = df11.sort_index()
 
 # Renaming all the dataframes for clarity
 total_graduands = df1
-intake_gender_2023 = df2
-completion_rate_2022 = df3
-completion_rate_2023 = df4
-overall_graduands_2025 = df5
-grads_2025_gender = df6
-msc_dsa_grad_list = df7
-pending_students_2023 = df8
-msc_dsa_pending_current = df9
-backlog_2024 = df10
-backlog_2023 = df11
+total_graduands = total_graduands.iloc[:10, :10]
 
-# Displaying the selected table
+intake_gender_2023 = df2
+intake_gender_2023 = intake_gender_2023.iloc[:4, :3]
+
+completion_rate_2022 = df3
+completion_rate_2022 = completion_rate_2022.iloc[:4,]
+
+completion_rate_2023 = df4
+completion_rate_2023 = completion_rate_2023.iloc[:4,]
+
+overall_graduands_2025 = df5
+overall_graduands_2025 = overall_graduands_2025.iloc[:4, :]
+
+grads_2025_gender = df6
+grads_2025_gender = grads_2025_gender.iloc[:4, :3]
+
+msc_dsa_grad_list = df7
+msc_dsa_grad_list = msc_dsa_grad_list.loc[:, ~msc_dsa_grad_list.columns.duplicated()]
+msc_dsa_grad_list.set_index('S/No.', inplace=True)
+
+pending_students_2023 = df8
+pending_students_2023 = pending_students_2023.iloc[:4, :5]
+
+df9.columns = df9.iloc[0, :].values
+df9 = df9.iloc[1:]
+msc_dsa_pending_current = df9
+msc_dsa_pending_current.set_index('S/No.', inplace=True)
+
+backlog_2024 = df10
+backlog_2024.set_index('S/No.', inplace=True)
+
+# Correcting df11 headers
+df11 = df11.loc[:, ~df11.columns.duplicated()]
+df11.loc[-1] = df11.columns           # Add current column names as a new row
+df11.index = df11.index + 1          # Shift all existing rows down
+df11 = df11.sort_index()
+backlog_2023 = df11
+backlog_2023.set_index('S/No.', inplace=True)
+
+# Visualization based on selected table
 if selected_table == "Table 1":
     st.subheader("Total Graduands")
-    st.dataframe(total_graduands)
+    st.write(total_graduands)
+elif selected_table == "Table 2":
+    st.subheader("Intake by gender (2023)")
+    st.write(intake_gender_2023)
+elif selected_table == "Table 3":
+    st.subheader("Completion Rate (2022)")
+    st.write(completion_rate_2022)
+elif selected_table == "Table 4":
+    st.subheader("Completion Rate (2023)")
+    st.write(completion_rate_2023)
+elif selected_table == "Table 5":
+    st.subheader("Overall Graduands (2025)")
+    st.write(overall_graduands_2025)
+elif selected_table == "Table 6":
+    st.subheader("Graduands by gnder (2025)")
+    st.write(grads_2025_gender)
+elif selected_table == "Table 7":
+    st.subheader("MSC DSA Graduation List")
+    st.write(msc_dsa_grad_list)
+elif selected_table == "Table 8":
+    st.subheader("Pending Students (2023)")
+    st.write(pending_students_2023)
+elif selected_table == "Table 9":
+    st.subheader("MSC DSA Pending Current")
+    st.write(msc_dsa_pending_current)
+elif selected_table == "Table 10":
+    st.subheader("MSC DSA Backlog (2024)")
+    st.write(backlog_2024)
+elif selected_table == "Table 11":
+    st.subheader("MSC DSA Backlog (2023)")
+    st.write(backlog_2023)
+else:
+    st.subheader("No data available for this table.")
+    st.write("Please select a valid table from the sidebar.")
