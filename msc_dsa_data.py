283107ad692383d00aa4729ff69196cee6a21765
Linet Gathuka <lynettg05@gmail.com>
2025-06-17 11:09:28 +0300

    app check

---
+++ msc_dsa_data.py
@@ -0,0 +1,30 @@
+import streamlit as st
+import docx
+import plotly.express as px
+import pandas as pd
+import numpy as np
+
+st.set_page_config(page_title="MSC DSA Graduation Visualization", layout="wide")
+
+#Loading the Word document
+doc = docx.Document("/Users/clementngatia/Downloads/1. Statistical Analysis/SIMS Masters Graduation Report_Class of 2025_Draft 1 (2).docx")
+
+@st.cache_data
+def load_data():
+    return docx.Document("/Users/clementngatia/Downloads/1. Statistical Analysis/SIMS Masters Graduation Report_Class of 2025_Draft 1 (2).docx") 
+
+doc = load_data()
+# Getting all tables
+tables = doc.tables
+print(f"Found {len(tables)} tables.")
+
+st.title("MSC DSA Graduation and Backlog Visualization")
+st.write("Welcome! This application visualizes the graduation and backlog data for the MSC DSA program.")
+
+with st.sidebar:
+    st.header("Navigation")
+    st.write(" Choose a table to visualize:")
+
+    table_options = [f"Table {i+1}" for i in range(len(tables))]
+    selected_table = st.selectbox("Select a table:", table_options)
+    
